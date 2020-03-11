1cb6c5c3126853d52cebd984b62255a55bb332e1
David Byström <david.bystrom@elev.ga.ntig.se>
2020-03-11 14:49:22 +0100

    Create article.js

---
+++ js/article.js
@@ -0,0 +1,9 @@
+class Article {
+    constructor(banner = new String(), headline = new String(), author = new String(), createdDate = new Date(), published = false) {
+        this.banner = banner;
+        this.headline = headline;
+        this.author = author;
+        this.createdDate = createdDate;
+        this.published = published;
+    }
+}
