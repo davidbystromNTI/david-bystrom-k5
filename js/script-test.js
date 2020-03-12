be3e621a11fccead99eacd36715a9699174b3014
David Byström <david.bystrom@elev.ga.ntig.se>
2020-03-12 10:51:04 +0100

    Add function buildArticle

--- js/script-test.js
+++ js/script-test.js
@@ -9,11 +9,13 @@ let selectNews = document.querySelector("#newsType");
 let fleraBilder = document.querySelector("#flerBilder");
 let inputBox = document.querySelector("#inputBox");
 
-function addArticle() {
+
+function createArticle() {
     //Read Picture input
     let pictureArray = readValuePictures();
     //Create new Article object with the input values
-    let article = new Article(bannerInput.value, rubrikInput.value, pictureArray, new Date(), true);
+    let article = new Article(bannerInput.value, rubrikInput.value, pictureArray, selectNews.value, new Date(), true);
+    return article;
 }
 
 function readValuePictures() {
@@ -25,4 +27,32 @@ function readValuePictures() {
     return pictureArray;
 }
 
-button.addEventListener("click", addArticle);
+function buildArticle() {
+    let article = createArticle();
+
+    //Create a div to act as a container and append it to div element
+    let container = document.createElement("div");
+    container.className += "container ";
+    element.appendChild(container);
+
+    //Create banner and text and append it to the container
+    let banner = document.createElement("h3");
+    let textBanner = document.createTextNode(article.banner);
+    let bannerContainer = document.createElement("div");
+    bannerContainer.className += "bannerContainer ";
+    container.appendChild(bannerContainer);
+    bannerContainer.appendChild(banner);
+    banner.appendChild(textBanner);
+
+    //Create header and text and append it to the container
+    let rubrik = document.createElement("h2");
+    let textRubrik = document.createTextNode(article.headline);
+    let rubrikContainer = document.createElement("div");
+    rubrikContainer.className += "rubrikContainer ";
+    container.appendChild(rubrikContainer);
+    rubrikContainer.appendChild(rubrik);
+    rubrik.appendChild(textRubrik);
+    
+
+}
+button.addEventListener("click", buildArticle);
