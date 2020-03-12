d36728403bf0a9640392df40166b96a0d90f2e7c
David Byström <david.bystrom@elev.ga.ntig.se>
2020-03-12 10:12:38 +0100

    Update article.js

--- js/article.js
+++ js/article.js
@@ -1,8 +1,8 @@
 export default class Article {
-    constructor(banner = new String(), headline = new String(), author = new String(), createdDate = new Date(), published = false) {
+    constructor(banner = new String(), headline = new String(), pictures = [], createdDate = new Date(), published = false) {
         this.banner = banner;
         this.headline = headline;
-        this.author = author;
+        this.pictures = pictures;
         this.createdDate = createdDate;
         this.published = published;
     }
