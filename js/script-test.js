218527380a55c6be390f64004678a8fe2f8f63a6
David Byström <david.bystrom@elev.ga.ntig.se>
2020-03-12 10:26:21 +0100

    Create function readValuePictures in script-test.js

--- js/script-test.js
+++ js/script-test.js
@@ -10,12 +10,19 @@ let fleraBilder = document.querySelector("#flerBilder");
 let inputBox = document.querySelector("#inputBox");
 
 function addArticle() {
-    //Import array of pictures URL
-    let bilderInput = document.querySelectorAll(".inputBilder");
-
+    //Read Picture input
+    let pictureArray = readValuePictures();
     //Create new Article object with the input values
-    let article = new Article(bannerInput.value, rubrikInput.value,  new Date(), true);
-    console.log(article);
+    let article = new Article(bannerInput.value, rubrikInput.value, pictureArray, new Date(), true);
+}
+
+function readValuePictures() {
+    let bilderInput = document.querySelectorAll(".inputBilder");
+    let pictureArray = [];
+    for (let i = 0; i < bilderInput.length; i++) {
+        pictureArray[i] = bilderInput[i].value;
+    }   
+    return pictureArray;
 }
 
 button.addEventListener("click", addArticle);
