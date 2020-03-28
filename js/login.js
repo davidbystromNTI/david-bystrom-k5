d0620d27e9368d1a93ff9e737d1440dd0f66fb85
David Byström <david.bystrom@elev.ga.ntig.se>
2020-03-28 12:38:40 +0100

    Create onLoad function in login.js

--- js/login.js
+++ js/login.js
@@ -1,2 +1,26 @@
 import Account from './account.js';
 
+const loginBtn = document.querySelector('#login');
+
+let accounts = [];
+
+function onLoad () {
+    //Load localStorage
+    let storageLocal = localStorage.getItem('account');
+    
+    //Check if localStorage is empty
+    if (storageLocal != null) {
+        //Parse to script
+        let loadedJson = JSON.parse(storageLocal);
+        
+        //Add loaded accounts to array accounts
+        accounts = loadedJson;
+    }
+}
+
+function login() {
+    
+}
+
+loginBtn.addEventListener('click', login);
+document.addEventListener('DOMContentLoaded', onLoad);
