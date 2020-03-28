ecf36367871bc9ccf31a0accd3efc306dd9ff870
David Byström <david.bystrom@elev.ga.ntig.se>
2020-03-28 13:08:22 +0100

    Finish login.js

--- js/login.js
+++ js/login.js
@@ -1,6 +1,8 @@
 import Account from './account.js';
 
 const loginBtn = document.querySelector('#login');
+const usernameInput = document.querySelector('#usernameInputLogin');
+const passwordInput = document.querySelector('#passwordInputLogin');
 
 let accounts = [];
 
@@ -15,11 +17,36 @@ function onLoad () {
         
         //Add loaded accounts to array accounts
         accounts = loadedJson;
+
+        
     }
 }
 
 function login() {
-    
+    //Check if username exists
+    let username = checkUsername();
+
+    //If not log error message 
+    if(username === false) {
+        console.log('There is no account assosiated with that username.');
+    }
+    else {
+        //Redirect person to tool
+        location.replace('tool.html');
+    }
+}
+
+function checkUsername () {
+    for (let i = 0; i < accounts.length; i++) {
+        if (accounts[i].username == usernameInput.value) return i;
+    }
+
+    return false;
+}
+
+function checkPassword (number) {
+    if (accounts[number].password == passwordInput.value) return true;
+    else return false;
 }
 
 loginBtn.addEventListener('click', login);
