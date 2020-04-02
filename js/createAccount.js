98f2ea91b0da9d21e3256118dc553bba71820c64
davidbystromNTI <david.bystrom@elev.ga.ntig.se>
2020-04-02 10:14:22 +0200

    Revert "Remove Account.js, createAccount.js and login.js"
    This reverts commit c862372056360d57bdc90ed02289a1703355d44c.

--- js/createAccount.js
+++ js/createAccount.js
@@ -1,30 +1,30 @@
-// import Account from './account.js';
+import Account from './account.js';
 
-// const createAccountBtn = document.querySelector('#createAccount');
-// const inputEmail = document.querySelector('#emailInput');
-// const inputUsername = document.querySelector('#usernameInput');
-// const inputPassword = document.querySelector('#passwordInput');
-// const firstNameInput = document.querySelector('#firstNameInput');
-// const lastNameInput = document.querySelector('#lastNameInput');
+const createAccountBtn = document.querySelector('#createAccount');
+const inputEmail = document.querySelector('#emailInput');
+const inputUsername = document.querySelector('#usernameInput');
+const inputPassword = document.querySelector('#passwordInput');
+const firstNameInput = document.querySelector('#firstNameInput');
+const lastNameInput = document.querySelector('#lastNameInput');
 
-// let accounts = [];
+let accounts = [];
 
-// function createAccount() {
-//     //Create object account with input data
-//     let account = new Account(inputUsername.value, inputEmail.value, inputPassword.value, firstNameInput.value, lastNameInput.value);
+function createAccount() {
+    //Create object account with input data
+    let account = new Account(inputUsername.value, inputEmail.value, inputPassword.value, firstNameInput.value, lastNameInput.value);
     
-//     //Add account to array accounts
-//     accounts.push(account);
+    //Add account to array accounts
+    accounts.push(account);
 
-//     //Make JSON of accounts
-//     let myJSON = JSON.stringify(accounts);
+    //Make JSON of accounts
+    let myJSON = JSON.stringify(accounts);
 
-//     //Add JSON to localstorage
-//     localStorage.setItem('account', myJSON);
+    //Add JSON to localstorage
+    localStorage.setItem('account', myJSON);
 
-//     console.log(localStorage);
+    console.log(localStorage);
 
-//     location.reload();
-// }
+    location.reload();
+}
 
-// createAccountBtn.addEventListener("click", createAccount);
+createAccountBtn.addEventListener("click", createAccount);
--- js/login.js
+++ js/login.js
@@ -1,53 +1,53 @@
-// import Account from './account.js';
+import Account from './account.js';
 
-// const loginBtn = document.querySelector('#login');
-// const usernameInput = document.querySelector('#usernameInputLogin');
-// const passwordInput = document.querySelector('#passwordInputLogin');
+const loginBtn = document.querySelector('#login');
+const usernameInput = document.querySelector('#usernameInputLogin');
+const passwordInput = document.querySelector('#passwordInputLogin');
 
-// let accounts = [];
+let accounts = [];
 
-// function onLoad () {
-//     //Load localStorage
-//     let storageLocal = localStorage.getItem('account');
+function onLoad () {
+    //Load localStorage
+    let storageLocal = localStorage.getItem('account');
     
-//     //Check if localStorage is empty
-//     if (storageLocal != null) {
-//         //Parse to script
-//         let loadedJson = JSON.parse(storageLocal);
+    //Check if localStorage is empty
+    if (storageLocal != null) {
+        //Parse to script
+        let loadedJson = JSON.parse(storageLocal);
         
-//         //Add loaded accounts to array accounts
-//         accounts = loadedJson;
+        //Add loaded accounts to array accounts
+        accounts = loadedJson;
 
         
-//     }
-// }
+    }
+}
 
-// function login() {
-//     //Check if username exists
-//     let username = checkUsername();
+function login() {
+    //Check if username exists
+    let username = checkUsername();
 
-//     //If not log error message 
-//     if(username === false) {
-//         console.log('There is no account assosiated with that username.');
-//     }
-//     else {
-//         //Redirect person to tool
-//         location.replace('tool.html');
-//     }
-// }
+    //If not log error message 
+    if(username === false) {
+        console.log('There is no account assosiated with that username.');
+    }
+    else {
+        //Redirect person to tool
+        location.replace('tool.html');
+    }
+}
 
-// function checkUsername () {
-//     for (let i = 0; i < accounts.length; i++) {
-//         if (accounts[i].username == usernameInput.value) return i;
-//     }
+function checkUsername () {
+    for (let i = 0; i < accounts.length; i++) {
+        if (accounts[i].username == usernameInput.value) return i;
+    }
 
-//     return false;
-// }
+    return false;
+}
 
-// function checkPassword (number) {
-//     if (accounts[number].password == passwordInput.value) return true;
-//     else return false;
-// }
+function checkPassword (number) {
+    if (accounts[number].password == passwordInput.value) return true;
+    else return false;
+}
 
-// loginBtn.addEventListener('click', login);
-// document.addEventListener('DOMContentLoaded', onLoad);
+loginBtn.addEventListener('click', login);
+document.addEventListener('DOMContentLoaded', onLoad);
