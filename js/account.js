c862372056360d57bdc90ed02289a1703355d44c
davidbystromNTI <david.bystrom@elev.ga.ntig.se>
2020-04-02 10:10:45 +0200

    Remove Account.js, createAccount.js and login.js

--- js/account.js
+++ js/account.js
@@ -1,9 +1,9 @@
-export default class Account {
-    constructor(username= new String(), email = new String(), password = new String(), firstName = new String(), lastName = new String()) {
-        this.username = username;
-        this.email = email;
-        this.password = password;
-        this.firstName = firstName;
-        this.lastName = lastName;
-    }
-}
+// export default class Account {
+//     constructor(username= new String(), email = new String(), password = new String(), firstName = new String(), lastName = new String()) {
+//         this.username = username;
+//         this.email = email;
+//         this.password = password;
+//         this.firstName = firstName;
+//         this.lastName = lastName;
+//     }
+// }
